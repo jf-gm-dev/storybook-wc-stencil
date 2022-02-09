b894e5f3d6e0af2966b2c2e779226b5aee5b8d1a
Fernando Gómez <jfernando.gomez@theksquaregroup.com>
2022-02-09 16:08:19 -0600

    Global styles for stencil

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'storybook-wc-stencil',
+  globalStyle: 'src/styles/globals.css',
   outputTargets: [
     {
       type: 'dist',
